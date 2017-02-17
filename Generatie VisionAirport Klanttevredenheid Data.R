64ccc4347b13bd1362900eca3fa67561ad1bd6b5
ithurlings <ingrid.thurlings@ordina.nl>
2017-02-17 15:44:36 +0100

    klanttevredenheid operatie, faciliteiten, shops

--- Generatie VisionAirport Klanttevredenheid Data.R
+++ Generatie VisionAirport Klanttevredenheid Data.R
@@ -1,6 +1,6 @@
 #selecteer benodigde kolommen om klanttevredenheid te berekenen en schrijf naar nieuwe tabel
-
-klanttevredenheid <- subset(sim, month(Datum) == 02, select = c("Vluchtnr", "Datum", "Bezetting", "Vertraging", "Gatewissel", "Terminal", "Destcode"))
+library(lubridate)
+klanttevredenheid <- subset(sim, month(Datum) == 02, select = c("Vluchtnr", "Datum", "Bezetting", "Vertraging", "Gatewissel", "Terminal", "Destcode", "Planuur"))
 
 #creeer kolommen
 klanttevredenheid <- mutate(klanttevredenheid, Operatie = 0)
@@ -12,6 +12,13 @@ klanttevredenheid <- mutate(klanttevredenheid, Operatie = 8 - 0.5 * Bezetting / 
 klanttevredenheid <- mutate(klanttevredenheid, Operatie = round(Operatie, digits = 1))
 
 #bereken klanttevredenheid Faciliteiten
-# 8 - 3*TerminalA - 1.5*TerminalB + Norm(0,0.5)
+klanttevredenheid <- mutate(klanttevredenheid, Faciliteiten = 8 - 3 * as.integer(Terminal == "A") - 1.5 * as.integer(Terminal == "B") + rnorm(length(Faciliteiten), 0, 0.5))
+klanttevredenheid <- mutate(klanttevredenheid, Faciliteiten = round(Faciliteiten, digits = 1))
 
+#bereken klanttevredenheid Shops
+#passagiers naar Alicante (ALC), zijn meer ontevreden
+#winkels zijn dicht tussen 23:00 en 6:00
 
+klanttevredenheid <- mutate(klanttevredenheid, Shops = rnorm(length(Shops), 6, 1) - 1 * as.integer(Destcode == "ALC"))
+klanttevredenheid$Shops[klanttevredenheid$Planuur > 23 | klanttevredenheid$Planuur < 6] <- NA
+klanttevredenheid <- mutate(klanttevredenheid, Shops = round(Shops, digits = 1))
